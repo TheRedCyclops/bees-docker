c4aa68fc930b8ec91f60573131ed47dba38ca0ed
TheRedCyclops <76230780+TheRedCyclops@users.noreply.github.com>
2026-02-26 21:00:26 +0100

    Quote variables in entrypoint to avoid unwanted splitting

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Trigger an error if non-zero exit code is encountered
 set -e 
-ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-mount /dev/mapper/$CACHEDEV /mnt -o subvol=/
-if [ ! -f ${HASH_TABLE} ]; then
+ln -snf /usr/share/zoneinfo/"$TZ" /etc/localtime && echo "$TZ" > /etc/timezone
+mount /dev/mapper/"$CACHEDEV" /mnt -o subvol=/
+if [ ! -f "${HASH_TABLE}" ]; then
 	mkdir -p /mnt/.beeshome
-	truncate -s ${HASH_TABLE_SIZE} ${HASH_TABLE}
-	chmod 700 ${HASH_TABLE}
+	truncate -s "${HASH_TABLE_SIZE}" "${HASH_TABLE}"
+	chmod 700 "${HASH_TABLE}"
 fi
 exec /bin/bees --no-timestamps "$@" /mnt 2>&1
